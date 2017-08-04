f78b4e32e4a11f734de7fad8ef247ca4e7fcdb46
Nikolay Ivanov <nikolay9001@gmail.com>
2017-08-04 15:49:02 +0300

    sw fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,15 @@ import './navigation';
 import './mailSending';
 
 if ('serviceWorker' in navigator) {
-  console.log('service worker exists');
+  // console.log('service worker exists');
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').
+    navigator.serviceWorker.register('./sw.js').
       then((registration) => {
       // registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, (err) => {
       // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+        // console.log('ServiceWorker registration failed: ', err);
       });
   });
 }
