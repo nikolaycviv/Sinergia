5ebac492df0a0694512fc637e8dee4761b47192e
Nikolay Ivanov <nikolay9001@gmail.com>
2017-08-09 17:22:53 +0300

    fav-icons fixed (webpack plugin added), removed src/css file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const path = require('path');
 const extractPlugin = new ExtractTextPlugin({
   filename: 'main.css'
@@ -69,6 +70,38 @@ module.exports = {
       minify: true,
       navigateFallback: `${path.resolve(__dirname, 'dist/index.html')}`,
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+    }),
+    new FaviconsWebpackPlugin({
+      // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+      background: '#fff',
+      // emit all stats of the generated icons
+      emitStats: false,
+      // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        windows: false,
+        yandex: false
+      },
+      // inject the html into the html-webpack-plugin
+      inject: true,
+      // your source logo
+      logo: './src/images/favicon.ico',
+      // generate a cache file with control hashes and
+      // don't rebuild the favicons until those hashes change
+      // persistentCache: true,
+      // the prefix for all image files (might be a folder or a name)
+      // prefix: 'icons-[hash]/',
+      // the name of the json containing all favicon information
+      // statsFilename: 'iconstats-[hash].json',
+      // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+      title: 'SinergiaPlus'
     })
   ]
 };
